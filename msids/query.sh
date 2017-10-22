077516ead8ed5a838a44b5cb02f3de3f587c148f
Grant Tremblay <granttremblay@gmail.com>
2017-10-22 19:19:03 -0400

    Initial commit

---
+++ msids/query.sh
@@ -0,0 +1,10 @@
+ska_fetch Point_SunCentAng --start=1999:207
+unzip fetch.zip
+rm fetch.zip
+ska_fetch Dist_SatEarth --start=1999:207
+unzip fetch.zip
+rm fetch.zip
+export ENG_ARCHIVE=/data/baffin/tom/git/eng_archive:/proj/sot/ska/data/eng_archive
+ska_fetch 2CE00ATM --start=1999:207
+unzip fetch.zip
+rm fetch.zip
